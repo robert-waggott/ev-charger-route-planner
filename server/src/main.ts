2c6077f79002c35413f0c911c1a0b09968155512
Robert Waggott <robert.waggott@gmail.com>
2021-07-29 21:36:26 +0100

    further scaffolding and introduction of env file

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as dotenv from "dotenv";
+import { join } from "path";
+
+dotenv.config({
+    path: join(__dirname, "..", "config.env")
+});
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    app.enableCors();
+
+    await app.listen(parseInt(process.env.PORT, 10) || 3001);
 }
 bootstrap();
