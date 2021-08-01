35590fa83ee2ff2b7fd18417f51970ce8f30435e
Robert Waggott <robert.waggott@gmail.com>
2021-08-01 19:59:01 +0100

    refactoring

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./controllers/app.controller";
 import { ConfigController } from "./controllers/config.controller";
 import { LocationSearchController } from "./controllers/location-search.controller";
-import { AppService } from "./services/app.service";
+import { LocationSearchService } from "./services/location-search.service";
 
 @Module({
     imports: [],
     controllers: [AppController, ConfigController, LocationSearchController],
-    providers: [AppService]
+    providers: [LocationSearchService]
 })
 export class AppModule {}
