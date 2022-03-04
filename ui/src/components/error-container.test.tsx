c553c4afaedc93ee5d6d1f547b78866b2e284126
Robert Waggott <robert.waggott@gmail.com>
2022-03-04 15:48:54 +0000

    starting to write tests

---
+++ ui/src/components/error-container.test.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { ErrorContainer } from "./error-container";
+
+test("Renders nothing when no error is passed in", () => {
+    const { container } = render(<ErrorContainer />);
+
+    expect(container.childElementCount).toEqual(0);
+});
+
+test("Renders the correct error when passed in", () => {
+    const error = "This is my error";
+    const { queryByText } = render(<ErrorContainer error={error} />);
+
+    expect(queryByText(error)).not.toBeNull();
+});
