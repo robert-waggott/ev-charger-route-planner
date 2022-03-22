3d4cae614f7b4b5a75c7666e56cdc3262e6ab242
Rob Waggott <robert.waggott@gmail.com>
2022-03-22 19:04:47 +0000

    more work on the settings modal

--- ui/src/components/settings.tsx
+++ ui/src/components/settings.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { Modal, Container, Button, Row, Col, Card } from "react-bootstrap";
 import { FaCog } from "react-icons/fa";
 import styled from "styled-components";
+import { TileOption } from "../classes/config";
 
 const StyledSettingsButton = styled.button`
     border: none;
@@ -19,20 +21,44 @@ export const SettingsCog = () => {
     const [showSettings, setShowSettings] = React.useState(false);
 
     return (
-        <StyledSettingsButton onClick={() => setShowSettings(true)}>
-            <FaCog />
-        </StyledSettingsButton>
+        <>
+            <StyledSettingsButton onClick={() => setShowSettings(true)}>
+                <FaCog />
+            </StyledSettingsButton>
+
+            <Modal
+                show={showSettings}
+                onHide={() => setShowSettings(false)}
+                backdrop="static"
+                size="lg"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Settings</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Container fluid className="g-0">
+                        <Row>
+                            {Object.values(TileOption).map((option) => (
+                                <Col sm={4}>
+                                    <Card>
+                                        <Card.Header>{option}</Card.Header>
+                                        <Card.Body>
+                                            <Card.Text></Card.Text>
+                                            <Card.Link href="#">Choose this route</Card.Link>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            ))}
+                        </Row>
+                    </Container>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShowSettings(false)}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
-};
-
-interface SettingsModalProps {
-    show: boolean;
-}
-
-const SettingsModal = (props: SettingsModalProps) => {
-    const [show, setShow] = React.useState(false);
-
-    React.useEffect(() => {
-        setShow(props.show);
-    }, [props.show]);
 };
